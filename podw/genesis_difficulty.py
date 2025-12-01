98c5f35916ef79f445ccbc6f7ad07e734eaf059b
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-30 23:34:02 -0600

    Create genesis_difficulty.py

---
+++ podw/genesis_difficulty.py
@@ -0,0 +1,10 @@
+from termcolor import colored
+import os
+
+hashrate = input(colored("Set a Hashrate: ", "white", attrs=["bold"]))
+os.system('cls' if os.name == 'nt' else 'clear')
+confirmation = input(colored("Set a Hex Confirmation Time: ", "white", attrs=["bold"]))
+os.system('cls' if os.name == 'nt' else 'clear')
+difficulty = 2**80 / (int(hashrate) * int(confirmation))
+target = 2**224 * 2**80 / difficulty
+print(colored(f"Numerical: {str(target)}\nHex: {hex(target)}", "white", attrs=["bold"])
