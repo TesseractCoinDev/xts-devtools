0bcbb4f20fdf719d3bd830e8305ac2f17bb9f045
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-12-01 01:36:40 -0600

    Create dynamic_target.py

---
+++ podw/dynamic_target.py
@@ -0,0 +1,9 @@
+from termcolor import colored
+import os
+
+elapsed = input(colored("Elapsed Time: ", "white", attrs=["bold"]))
+os.system('cls' if os.name == 'nt' else 'clear')
+old = input(colored("Old Target: ", "white", attrs=["bold"]))
+os.system('cls' if os.name == 'nt' else 'clear')
+target = round(2**224 * 2**80 / (round(2**224 * 2**80 / int(old)) * (int(elapsed) / 22)))
+print(colored(f"Numeric: {str(target)}\nHex: {target.to_bytes((target.bit_length() + 7) // 8, 'big').hex()}"))
