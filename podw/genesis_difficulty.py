a964392c2f7a73e45f544ec8550552b81a30ec75
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-30 23:42:54 -0600

    Update genesis_difficulty.py

--- podw/genesis_difficulty.py
+++ podw/genesis_difficulty.py
@@ -6,5 +6,6 @@ os.system('cls' if os.name == 'nt' else 'clear')
 confirmation = input(colored("Set a Hex Confirmation Time: ", "white", attrs=["bold"]))
 os.system('cls' if os.name == 'nt' else 'clear')
 difficulty = 2**80 / (int(hashrate) * int(confirmation))
-target = 2**224 * 2**80 / difficulty
-print(colored(f"Numerical: {str(target)}\nHex: {hex(target)}", "white", attrs=["bold"]))
+target = round(2**224 * 2**80 / difficulty)
+hexed = target.to_bytes((target.bit_length() + 7) // 8, "big").hex()
+print(colored(f"Numerical: {str(target)}\nHex: {hexed}", "white", attrs=["bold"]))
